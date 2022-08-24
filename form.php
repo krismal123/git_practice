826810ecdfe972b4d0983a95ca27a393a67eae00
Dimantha Krismal <dimantha.krismal@gmail.com>
2022-08-24 13:09:41 +0100

    first commit

---
+++ form.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Upload Image</title>
+</head>
+<body>
+    <centre>
+        <h1> Upload Image </h1>
+        <form action="" method="POST" enctype="multipart/form-data">
+
+
+            <label> Choose a Image :</label> <br>
+            <input type ="file" name= "image" id="image"/> <br>
+
+            <label> Enter Your Name :</label> <br>
+            <input type ="text" name= "Name" placeholder="Your Name"/> <br>
+
+            <label> Message :</label> <br>
+            <input type ="text" name= "Message" placeholder="Message"/> <br>
+
+            <input type ="submit" name= "upload" value="Upload Image/Data"/> <br>
+
+            </form>
+
+      </centre>
+<body>
+</html>
